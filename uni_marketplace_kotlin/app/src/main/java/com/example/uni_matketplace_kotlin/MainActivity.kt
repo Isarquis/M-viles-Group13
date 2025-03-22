8da06da471338bfc0199af358bfc9cf5c0204734
Isarquis <123596680+Isarquis@users.noreply.github.com>
2025-03-22 01:28:49 -0500

    Activity to frame

--- uni_marketplace_kotlin/app/src/main/java/com/example/uni_matketplace_kotlin/MainActivity.kt
+++ uni_marketplace_kotlin/app/src/main/java/com/example/uni_matketplace_kotlin/MainActivity.kt
@@ -29,15 +29,17 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_map
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
-    fun abrirMapa(view: View){
-        val intent= Intent(this, MapsActivity::class.java).apply{ }
-        startActivity(intent)
-    }
+
 
 }
